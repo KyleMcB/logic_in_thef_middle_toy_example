947268104eed6cc8e1a8b9fee8d66235ef9aee45
Kyle McBurnett <kmcburnett@microsoft.com>
2022-09-28 17:50:59 -0700

    can not construct view model

---
+++ app/src/test/java/com/example/untestable/UntestableViewModelTest.kt
@@ -0,0 +1,11 @@
+package com.example.untestable
+
+import org.junit.Test
+
+
+internal class UntestableViewModelTest {
+    @Test
+    fun basic() {
+        val subject = UntestableViewModel()
+    }
+}
